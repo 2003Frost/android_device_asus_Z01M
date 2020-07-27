001f3f96403d87841cbf7f195b4cbc8f1709efe8
Seplx <superseplex@protonmail.com>
2020-07-27 14:15:16 +0200

    Revert "remove calls already made in aosp_Z01M.mk"
    This reverts commit 82a17b30d1f75eac2d61ea2c0b9838e9a32316b0.

--- device.mk
+++ device.mk
@@ -13,6 +13,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+$(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
+
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
