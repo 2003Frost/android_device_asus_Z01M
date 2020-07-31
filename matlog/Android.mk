dfd89f431a2268f1f536fc1bc82bf32576d29a0a
Seplx <superseplex@protonmail.com>
2020-07-31 16:32:38 +0200

    fix matlog path

--- matlog/Android.mk
+++ matlog/Android.mk
@@ -2,7 +2,7 @@ LOCAL_PATH := $(call my-dir)
 
 include $(CLEAR_VARS)
 LOCAL_MODULE := Matlog
-LOCAL_SRC_FILES := matlog/com.pluscubed.matlog-1.2.3.apk
+LOCAL_SRC_FILES := com.pluscubed.matlog-1.2.3.apk
 LOCAL_MODULE_SUFFIX := .apk
 LOCAL_MODULE_CLASS := APPS
 LOCAL_CERTIFICATE := PRESIGNED
