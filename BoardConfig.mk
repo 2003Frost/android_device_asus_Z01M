5c32c2f88ffa6fe3f8a9dfb04c69b344c329eb1d
Seplx <superseplex@protonmail.com>
2020-08-10 13:19:09 +0200

    add cryptfs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -118,8 +118,8 @@ TARGET_TAP_TO_WAKE_NODE := "/sys/bus/i2c/devices/i2c-3/3-0038/dclick_mode"
 
 #TODO - Do we need this? Does AOSP have disk encryption without this?
 # Crypto
-#TARGET_HW_DISK_ENCRYPTION := true
-#TARGET_CRYPTFS_HW_PATH := device/qcom/common/cryptfs_hw
+TARGET_HW_DISK_ENCRYPTION := true
+TARGET_CRYPTFS_HW_PATH := $(DEVICE_PATH)/cryptfs_hw
 
 # Display
 USE_OPENGL_RENDERER := true
