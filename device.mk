bbedef70209ae80eedd1365817d648db894af165
Seplx <superseplex@protonmail.com>
2020-09-26 20:51:09 +0200

    add stock media files

--- device.mk
+++ device.mk
@@ -377,9 +377,6 @@ PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/media/capability.xml:system/etc/capability.xml \
-
 # OMX
 PRODUCT_PACKAGES += \
     libc2dcolorconvert \
