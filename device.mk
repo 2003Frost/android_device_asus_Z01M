b510f56e6818a06658377288fd6c0de8c3139b69
2003Frost <davisantos.lorde@gmail.com>
2020-09-26 16:51:49 -0300

    Added missing separators and removed extra line

--- device.mk
+++ device.mk
@@ -148,8 +148,8 @@ PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
-    $(LOCAL_PATH)/configs/audio/mixer_paths_skuk.xml:system/etc/audio_policy_non_eu_volumes_Z01M.xml
-    $(LOCAL_PATH)/configs/audio/mixer_paths_skuk.xml:system/etc/audio_policy_volumes_Z01M.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths_skuk.xml:system/etc/audio_policy_non_eu_volumes_Z01M.xml \
+    $(LOCAL_PATH)/configs/audio/mixer_paths_skuk.xml:system/etc/audio_policy_volumes_Z01M.xml \
     $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/configs/audio/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
@@ -203,7 +203,6 @@ PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/camera/imx362_chromatix.xml:system/etc/camera/imx362_chromatix.xml \
     $(LOCAL_PATH)/configs/camera/imx362_chromatix_factory.xml:system/etc/camera/imx362_chromatix_factory.xml \
     $(LOCAL_PATH)/configs/camera/msm8953_camera_ZD552KL_PHOENIX.xml:system/etc/camera/msm8953_camera_ZD552KL_PHOENIX.xml \
-
     $(LOCAL_PATH)/configs/camera/VStab_1280x720_30_0.conf:system/etc/camera/VStab_1280x720_30_0.conf \
     $(LOCAL_PATH)/configs/camera/VStab_1280x720_30_1.conf:system/etc/camera/VStab_1280x720_30_1.conf \
     $(LOCAL_PATH)/configs/camera/VStab_1280x720_30_2.conf:system/etc/camera/VStab_1280x720_30_2.conf \
