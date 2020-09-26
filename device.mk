12635663056d266695a29c2bf96544ae3913e811
Seplx <superseplex@protonmail.com>
2020-09-26 19:28:50 +0200

    add more stock audio configs

--- device.mk
+++ device.mk
@@ -148,6 +148,8 @@ PRODUCT_COPY_FILES +=  \
     $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:system/etc/audio_platform_info.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml \
     $(LOCAL_PATH)/configs/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/configs/audio/mixer_paths_skuk.xml:system/etc/audio_policy_non_eu_volumes_Z01M.xml
+    $(LOCAL_PATH)/configs/audio/mixer_paths_skuk.xml:system/etc/audio_policy_volumes_Z01M.xml
     $(LOCAL_PATH)/configs/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
     $(LOCAL_PATH)/configs/audio/audio_output_policy.conf:system/vendor/etc/audio_output_policy.conf \
     $(LOCAL_PATH)/configs/audio/audio_platform_info_extcodec.xml:system/etc/audio_platform_info_extcodec.xml \
